72610f7afbfad84bea33803c6f547c6262722dd4
netpipe <teslaenergy@gmail.com>
2022-12-09 12:19:05 -0700

    resource

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QPixmap m("./Resource/fired.jpg");
+
+        QFile file("./Resource/orange.qss");
+        file.open(QFile::ReadOnly);
+        QString styleSheet = QLatin1String(file.readAll());
+    w.setStyleSheet(styleSheet);
+
+QSplashScreen splash(m);
+splash.show();
+
+
     w.show();
     return a.exec();
 }
